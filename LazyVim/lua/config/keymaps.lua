36ec239bede95101b08d563d676b2b31628d941b
Max Klammer <maximilian.klammer@gmail.com>
2023-12-12 14:20:58 +0100

    working keymaps

--- LazyVim/lua/config/keymaps.lua
+++ LazyVim/lua/config/keymaps.lua
@@ -3,10 +3,6 @@
 -- Add any additional keymaps here
 
 return {
-  n = {
-    ["<C-d>"] = { "<C-d>zz", desc = "Scroll down" },
-    ["<C-u>"] = { "<C-u>zz", desc = "Scroll up" },
-    ["<S-l>"] = { "<cmd>bnext<cr>", desc = "Next Buffer" },
-    ["<S-h>"] = { "<cmd>bprev<cr>", desc = "Previous Buffer" },
-  },
+  vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" }),
+  vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" }),
 }
